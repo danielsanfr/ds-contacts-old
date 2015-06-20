4134978f3af4651aa04ffbe7815bf3cfeb3a3a0a
Daniel San <daniel.sam123@hotmail.com>
2015-06-20 02:30:14 -0300

    Initiated the move to use the Android Design Support Library.
    We are already using the NavigationView in DrawerLayout.
    
    Signed-off-by: Daniel San <daniel.sam123@hotmail.com>

--- app/src/main/java/br/com/danielsan/dscontacts/activities/MainActivity.java
+++ app/src/main/java/br/com/danielsan/dscontacts/activities/MainActivity.java
@@ -1,24 +1,43 @@
 package br.com.danielsan.dscontacts.activities;
 
+import android.content.res.Configuration;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import br.com.danielsan.dscontacts.fragments.MainFragment;
-import br.com.danielsan.dscontacts.fragments.NavigationDrawerFragment;
 import br.com.danielsan.dscontacts.R;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
-public class MainActivity extends BaseActivity
-        implements  NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class MainActivity extends BaseActivity implements OnNavigationItemSelectedListener {
 
     private CharSequence mTitle;
     private MainFragment mMainFragment;
-    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @InjectView(R.id.drw_lyt_main)
+    protected DrawerLayout mDrawerLayout;
+    @InjectView(R.id.nvgt_vw)
+    protected NavigationView mNavigationView;
+    @InjectView(R.id.txt_vw_user_name)
+    protected TextView mUserNameTextView;
+    @InjectView(R.id.txt_vw_user_email)
+    protected TextView mUserEmailTextView;
+    @InjectView(R.id.img_vw_avatar)
+    protected ImageView mAvatarImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +46,43 @@ public class MainActivity extends BaseActivity
         ButterKnife.inject(this);
 
         mTitle = this.getTitle();
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                                                  R.string.navigation_drawer_open,
+                                                  R.string.navigation_drawer_close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                supportInvalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                supportInvalidateOptionsMenu();
+            }
+        };
+
+        mActionBar.setHomeButtonEnabled(true);
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState();
+            }
+        });
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
         // enable status bar tint
         this.getSystemBarTintManager().setStatusBarTintEnabled(true);
         // enable navigation bar tint
         this.getSystemBarTintManager().setNavigationBarTintEnabled(true);
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                this.getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp((DrawerLayout) this.findViewById(R.id.drawer_layout));
+        mNavigationView.setNavigationItemSelectedListener(this);
+        mUserNameTextView.setText("Daniel San");
+        mUserEmailTextView.setText("daniel.samrocha@gmail.com");
+        mAvatarImageView.setImageResource(R.drawable.img_avatar);
 
         mMainFragment = new MainFragment();
         this.replaceFragment(mMainFragment);
@@ -49,6 +94,19 @@ public class MainActivity extends BaseActivity
     }
 
     @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        menuItem.setChecked(true);
+        mDrawerLayout.closeDrawer(mNavigationView);
+        return true;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         if (mMainFragment != null)
@@ -56,31 +114,19 @@ public class MainActivity extends BaseActivity
     }
 
     @Override
-    public void onNavigationDrawerItemSelected(int position) {
-    }
-
-    public void restoreActionBar() {
-        mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        mActionBar.setDisplayShowTitleEnabled(true);
-        mActionBar.setTitle(mTitle);
-    }
-
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
+        if (mDrawerLayout.isDrawerOpen(mNavigationView))
+            this.getMenuInflater().inflate(R.menu.global, menu);
+        else
             this.getMenuInflater().inflate(R.menu.main, menu);
-            this.restoreActionBar();
-            return true;
-        }
+
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item))
+            return true;
         switch (item.getItemId()) {
             //noinspection SimplifiableIfStatement
             case R.id.mn_create_group:
@@ -94,5 +140,4 @@ public class MainActivity extends BaseActivity
         this.getSystemBarTintManager().setTintColor(newColor);
         mActionBar.setBackgroundDrawable(new ColorDrawable(newColor));
     }
-
 }
