9b815e9516d6f5bc14b1a7bd215b7f7ca049f260
Daniel San <daniel.sam123@hotmail.com>
2015-06-21 14:10:55 -0300

    Complete change in the application styles.
    Also added the CoordinatorLayout the screen contact information.
    
    Signed-off-by: Daniel San <daniel.sam123@hotmail.com>

--- app/src/main/java/br/com/danielsan/dscontacts/activities/ContactInfoActivity.java
+++ app/src/main/java/br/com/danielsan/dscontacts/activities/ContactInfoActivity.java
@@ -1,7 +1,10 @@
 package br.com.danielsan.dscontacts.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.r0adkll.slidr.Slidr;
 
@@ -10,22 +13,34 @@ import br.com.danielsan.dscontacts.R;
 /**
  * Created by daniel on 15/06/15.
  */
-public class ContactInfoActivity extends BaseActivity {
+public class ContactInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_contact_info);
         Slidr.attach(this);
+
+        Intent intent = getIntent();
+        final String cheeseName = "Daniel San";
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        toolbar.setPadding(0, this.getStatusBarHeight(), 0, 0);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(cheeseName);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-    }
+    // A method to find height of the status bar
+    private int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = this.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0)
+            result = this.getResources().getDimensionPixelSize(resourceId);
+        return result;
+}
 
-    @Override
-    protected int getMasterContainer() {
-        return 0;
-    }
 }
