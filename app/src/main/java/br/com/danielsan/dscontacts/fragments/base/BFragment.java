f9a8c654f8e1778f147337812eee6bf1addbbd39
Daniel San <daniel.sam123@hotmail.com>
2015-08-03 02:57:22 -0300

    Improved class BFragmet.
    BFragmet now has a callback that notifies an activity when it was created.
    
    Signed-off-by: Daniel San <daniel.sam123@hotmail.com>

--- app/src/main/java/br/com/danielsan/dscontacts/fragments/base/BFragment.java
+++ app/src/main/java/br/com/danielsan/dscontacts/fragments/base/BFragment.java
@@ -18,23 +18,48 @@ import br.com.danielsan.dscontacts.util.FragmentsTransaction;
 public class BFragment extends Fragment {
 
     protected BaseActivity mBaseActivity;
+    private OnCreationCompleteListener mOnCreationCompleteListener;
 
     public BFragment() {}
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
+
+        if (activity instanceof OnCreationCompleteListener)
+            mOnCreationCompleteListener = (OnCreationCompleteListener) activity;
+        else
+            mOnCreationCompleteListener = null;
+
         try {
             mBaseActivity = (BaseActivity) activity;
         } catch (ClassCastException e) {
-            throw new ClassCastException("Activity must extend BaseActivity.");
+            throw new ClassCastException(activity.toString() + " must extend BaseActivity.");
         }
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (mOnCreationCompleteListener != null)
+            mOnCreationCompleteListener.onComplete(this);
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
         mBaseActivity = null;
+        mOnCreationCompleteListener = null;
+    }
+
+    /**
+     * Callbacks interface that notifies the activity that this fragment was created.
+     */
+    public interface OnCreationCompleteListener {
+        /**
+         * Called when the fragment was created.
+         */
+        void onComplete(BFragment fragment);
     }
 
 }
