2a325a1f4b9e1479964478840a1977fd0c0ce191
Daniel San <daniel.sam123@hotmail.com>
2015-08-03 03:09:47 -0300

    Removed unused imports.
    Signed-off-by: Daniel San <daniel.sam123@hotmail.com>

--- app/src/main/java/br/com/danielsan/dscontacts/fragments/base/BFragment.java
+++ app/src/main/java/br/com/danielsan/dscontacts/fragments/base/BFragment.java
@@ -1,16 +1,11 @@
 package br.com.danielsan.dscontacts.fragments.base;
 
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
 
-import com.readystatesoftware.systembartint.SystemBarTintManager;
-
 import br.com.danielsan.dscontacts.activities.BaseActivity;
-import br.com.danielsan.dscontacts.activities.MainActivity;
-import br.com.danielsan.dscontacts.util.FragmentsTransaction;
 
 /**
  * Created by daniel on 06/06/15.
